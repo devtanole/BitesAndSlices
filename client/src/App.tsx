7ef073b8f0571ddf8e648014ba1b6a5665afaa96
Devin Olesen <devintanielu@gmail.com>
2025-05-22 10:11:08 +0000

    auth page front end

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,18 @@
 import './App.css';
+import { UserProvider } from './components/UserContext';
+import { Route, Routes } from 'react-router-dom';
+import { Header } from './components/Header';
+import { AuthPage } from './pages/AuthPage';
 
 export default function App() {
-  return <></>;
+  return (
+    <UserProvider>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route path="/auth/sign-up" element={<AuthPage mode="sign-up" />} />
+          <Route path="/auth/sign-in" element={<AuthPage mode="sign-in" />} />
+        </Route>
+      </Routes>
+    </UserProvider>
+  );
 }
